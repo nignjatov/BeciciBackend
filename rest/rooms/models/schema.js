101403ba255aa92d63d29f15745cf20023b7d5e2
Nemanja Ignjatov <nemanja.ignjatov@rt-rk.com>
2016-01-23 16:09:07 +0100

    Changed room schema

--- rest/rooms/models/schema.js
+++ rest/rooms/models/schema.js
@@ -1,68 +1,69 @@
 var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
+var Schema = mongoose.Schema;
 
 var RoomSchema = new Schema({
-  title: {
-    type: Schema.Types.Mixed,
-    required: true
-  },
-  type: {
-    type: Schema.Types.Mixed,
-    required: true
-  },
-  description: {
-    type: Schema.Types.Mixed,
-    required: true
-  },
-  child_discount: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 100
-  },
-  child_bed_discount: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 100
-  },
-  image: String,
-  available: [{
-    from: {
-      type: Date,
-      required: true
+    title: {
+        type: Schema.Types.Mixed,
+        required: true
     },
-    to: {
-      type: Date,
-      required: true
+    type: {
+        type: Schema.Types.Mixed,
+        required: true
     },
-    amount: {
-      type: Number,
-      required: true
+    description: {
+        type: Schema.Types.Mixed,
+        required: true
     },
-    price: {
-      type: Number,
-      required: true
+    child_discount: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100
     },
-  }],
-  bed_number: Number,
-  big_bed : Boolean,
-  free_services: [{
-    type: Schema.Types.Mixed
-  }],
-  created_at: {
-    type: Date,
-    default: Date.now
-  },
-  last_modified: {
-    type: Date,
-  }
+    child_bed_discount: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100
+    },
+    image: String,
+    available: [{
+        from: {
+            type: Date,
+            required: true
+        },
+        to: {
+            type: Date,
+            required: true
+        },
+        amount: {
+            type: Number,
+            required: true
+        },
+        remained : {
+            type : Number,
+            required : true,
+        },
+        price: {type: Schema.Types.Mixed},
+    }],
+    bed_number: Number,
+    big_bed: Boolean,
+    free_services: [{
+        type: Schema.Types.Mixed
+    }],
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    last_modified: {
+        type: Date,
+    }
 });
 
-RoomSchema.pre('save', function(next){
-  now = new Date();
-  this.last_modified = now;
-  next();
+RoomSchema.pre('save', function (next) {
+    now = new Date();
+    this.last_modified = now;
+    next();
 });
 
 module.exports = RoomSchema;
