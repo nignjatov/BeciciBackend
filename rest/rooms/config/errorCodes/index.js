0b9db655f6628becbcdf96c6690fec5ca7c59805
OrahKokos <marko.paroski.ns@gmail.com>
2016-02-03 20:10:06 +0000

    working rooms + reservations

--- rest/rooms/config/errorCodes/index.js
+++ rest/rooms/config/errorCodes/index.js
@@ -3,5 +3,10 @@ module.exports = {
     httpCode: 404,
     message: "There is no such room",
     errorServerity: "green"
+  },
+  'ROOM_NOT_AVAILABLE': {
+  	httpCode: 404,
+  	message: "Sorry this room is not available anymore",
+  	errorServerity: "green"
   }
 }
