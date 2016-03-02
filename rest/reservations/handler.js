e3674d3f2f6042167d59c25a9ed5f7434d8f8f34
OrahKokos <marko.paroski.ns@gmail.com>
2016-03-02 21:22:59 +0100

    error handle

--- rest/reservations/handler.js
+++ rest/reservations/handler.js
@@ -70,71 +70,30 @@ module.exports = (function () {
       } 
       if (action == 'reject') {
         // reject
-        var options = {
-          url: 'http://194.106.182.81/test_app/' + action,
-          method: 'POST',
-          headers: {
-            token: process.env.INTESA_TOKEN
-          },
-          form: {
-            paymentId: paymentId
-          }
-        }
-        return request(options, function (err, response, body) {
-          if (err) return next(err);
-          Container.models['rooms'].findOne({'_id': req.body.order.room}, function (err, found) {
-            if (err) return next('MONGO_ERROR');
-            if (!found) return next('ROOM_NOT_FOUND');
-            var termin = _.find(found.available, function (ter) {
-              if (ter._id == req.body.order.termin){
-                return true
+        Container.models['reservations'].findOne({paymentId: paymentId}, function (err, reservation) {
+          if (reservation && reservation.status == 'APPROVED') {
+            var options = {
+              url: 'http://194.106.182.81/test_app/' + action,
+              method: 'POST',
+              headers: {
+                token: process.env.INTESA_TOKEN
+              },
+              form: {
+                paymentId: paymentId
               }
-              return false;
-            });
-            Container.models['reservations'].findOne({paymentId: paymentId}, function (err, reservation) {
-              return Container.email.send('reject', 
-                  {
-                    reservation: JSON.stringify(reservation, null, 2), 
-                    room: JSON.stringify(found, null, 2), 
-                    termin: JSON.stringify(termin, null, 2)
-                  }, 
-                  req.body.email, 
-                  function (err) {
-                    if (err) return next(err);
-                    res.sendStatus(200);
-                    return next();    
-                  }
-                );
-              });
-            });
-        });
-      }
-
-      if (action == 'cancel') {
-          var options = {
-            url: 'http://194.106.182.81/test_app/' + action,
-            method: 'POST',
-            headers: {
-              token: process.env.INTESA_TOKEN
-            },
-            form: {
-              paymentId: paymentId
             }
-          }
-          return request(options, function (err, response, body) {
-            if (err) return next(err);
-            Container.models['rooms'].findOne({'_id': req.body.order.room}, function (err, found) {
-              var termin = _.find(found.available, function (ter) {
-                if (ter._id == req.body.order.termin){
-                  return true
-                }
-                return false;
-              }); 
-              termin.remained++;
-              return found.save(function (err) {
+            return request(options, function (err, response, body) {
+              if (err) return next(err);
+              Container.models['rooms'].findOne({'_id': req.body.order.room}, function (err, found) {
                 if (err) return next('MONGO_ERROR');
-                Container.models['reservations'].findOne({paymentId: paymentId}, function (err, reservation) {
-                  return Container.email.send('cancel', 
+                if (!found) return next('ROOM_NOT_FOUND');
+                var termin = _.find(found.available, function (ter) {
+                  if (ter._id == req.body.order.termin){
+                    return true
+                  }
+                  return false;
+                });
+                return Container.email.send('reject', 
                     {
                       reservation: JSON.stringify(reservation, null, 2), 
                       room: JSON.stringify(found, null, 2), 
@@ -146,12 +105,62 @@ module.exports = (function () {
                       res.sendStatus(200);
                       return next();    
                     }
-                  );  
+                  );
+                });
+            });
+          } else {
+            return next('OPERATION_NOT_ALLOWED')
+          }
+        });
+      }
+
+      if (action == 'cancel') {
+        Container.models['reservations'].findOne({paymentId: paymentId}, function (err, reservation) {
+          if (reservation && reservation.status == 'CAPTURED') {
+            var options = {
+              url: 'http://194.106.182.81/test_app/' + action,
+              method: 'POST',
+              headers: {
+                token: process.env.INTESA_TOKEN
+              },
+              form: {
+                paymentId: paymentId
+              }
+            }
+            return request(options, function (err, response, body) {
+              if (err) return next(err);
+              Container.models['rooms'].findOne({'_id': req.body.order.room}, function (err, found) {
+                var termin = _.find(found.available, function (ter) {
+                  if (ter._id == req.body.order.termin){
+                    return true
+                  }
+                  return false;
+                }); 
+                termin.remained++;
+                return found.save(function (err) {
+                  if (err) return next('MONGO_ERROR');
+                  
+                    return Container.email.send('cancel', 
+                      {
+                        reservation: JSON.stringify(reservation, null, 2), 
+                        room: JSON.stringify(found, null, 2), 
+                        termin: JSON.stringify(termin, null, 2)
+                      }, 
+                      req.body.email, 
+                      function (err) {
+                        if (err) return next(err);
+                        res.sendStatus(200);
+                        return next();    
+                      }
+                    );
                 });
               });
             });
-          });
-      } 
+          } else {
+            return next('OPERATION_NOT_ALLOWED')
+          }
+        });    
+      }
     },
     deleteReservation: function (req, res, next) {
       var reservationId = req.params.reservationId;
